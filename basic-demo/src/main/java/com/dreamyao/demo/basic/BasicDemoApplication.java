36e65317d731971d75ece3eaa70ea765c05efc7b
brucelong <408527015@qq.com>
2018-07-28 22:11:18 +0800

    add basic demo

---
+++ basic-demo/src/main/java/com/dreamyao/demo/basic/BasicDemoApplication.java
@@ -0,0 +1,12 @@
+package com.dreamyao.demo.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicDemoApplication.class, args);
+    }
+}
